878a3490c32234e312071e000217d648937db99c
KeianBarton <Keian.Barton@Civica.co.uk>
2018-01-23 11:22:27 +0000

    navigating to routes from code

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
 
 import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { EventService } from './events/shared/event.service'
-import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { appRoutes } from './routes';
-import { RouterModule } from '@angular/router';
+import { ToastrService } from './common/toastr.service'
+import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { appRoutes } from './routes'
+import { CreateEventComponent } from './events/create-event.component'
 
 @NgModule({
     imports: [
@@ -21,6 +22,7 @@ import { RouterModule } from '@angular/router';
         EventsListComponent,
         EventThumbnailComponent,
         EventDetailsComponent,
+        CreateEventComponent,
         NavBarComponent
     ],
     providers: [EventService, ToastrService],
